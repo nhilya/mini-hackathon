62fba85476e183ce6c8e92e450ca86fc633e0195
zarinzaidi <aereinzayed@yahoo.com>
2024-03-31 18:41:36 +0800

    routing and navigation bar

---
+++ groupone/src/components/Navbar.js
@@ -0,0 +1,35 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+
+function Navbar() {
+    return (
+        <div>
+            <nav class="navbar navbar-expand-lg navbar-light bg-light">
+                {/* logo on navbar redirect to home */}
+                <a class="navbar-brand"><Link to="/home" class="nav-link"><img src='logo.png' style={{ width: '100px' }} /></Link></a>
+
+                {/* hamburger menu */}
+                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+
+                {/* navigation redirect to differenet components */}
+                <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                    <ul class="navbar-nav mr-auto">
+                        <li class="nav-item active">
+                            <Link to="/home" class="nav-link">Home</Link>
+                        </li>
+                        <li class="nav-item active">
+                            <Link to="/contact" class="nav-link">Contact</Link>
+                        </li>
+                        <li class="nav-item active">
+                            <Link to="/gallery" class="nav-link">Gallery</Link>
+                        </li>
+                    </ul>
+                </div>
+            </nav>
+        </div>
+    )
+}
+
+export default Navbar
