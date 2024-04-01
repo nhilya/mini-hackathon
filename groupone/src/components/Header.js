acc98784c949e1ea5bd381fa653e88a4fba75e6b
zarinzaidi <aereinzayed@yahoo.com>
2024-04-01 14:41:08 +0800

    comments on header updated

--- groupone/src/components/Header.js
+++ groupone/src/components/Header.js
@@ -1,7 +1,11 @@
 import React from 'react'
 
+// Header component for displaying the search bar on home
 function Header(props) {
+    // Destructure props to get the necessary values
     const { search, onInputChange, onSearchClick } = props;
+
+    // Render the header content
     return (
         <div className="jumbotron mb-0">
             <div className="container text-center">
@@ -14,10 +18,12 @@ function Header(props) {
                         type="text"
                         className="form-control w-50 mr-sm-2"
                         placeholder="Search Your Recipe..."
-                        value={search}
-                        onChange={onInputChange}
+                        value={search} // Value of the search input
+                        onChange={onInputChange} // Function to handle input change
                     />
+                    {/* Append the search button */}
                     <div className="input-group-append">
+                        {/* Button to search for recipes */}
                         <button className="btn btn-dark btn-outline-light" onClick={onSearchClick}>
                             Search Recipe
                         </button>
